74ce6cd498bbe10ce24f7c0921c1ae63ae2cf8eb
Ruan França <ruanzinr1000@hotmail.com>
2023-07-04 22:38:57 -0300

    rest pt2

--- src/main/java/br/com/buzzi/campeonatobrasileiro/rest/TimeRestController.java
+++ src/main/java/br/com/buzzi/campeonatobrasileiro/rest/TimeRestController.java
@@ -5,8 +5,7 @@ import br.com.buzzi.campeonatobrasileiro.service.TimeServico;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +15,20 @@ public class TimeRestController {
 
     @Autowired
     private TimeServico timeServico;
+
+    @GetMapping
     public ResponseEntity<List<Time>> getTimes(){
         return ResponseEntity.ok().body(timeServico.listarTimes());
     }
 
-    public ResponseEntity<Time> getTime (Integer id){
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Time> getTime (@PathVariable Integer id){
         return ResponseEntity.ok().body(timeServico.obterTime(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> cadastrarTime(Time time){
+        timeServico.cadastrarTime(time);
+        return ResponseEntity.ok().build();
     }
 }
