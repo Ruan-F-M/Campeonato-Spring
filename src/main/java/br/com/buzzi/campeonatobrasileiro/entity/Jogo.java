db12f7a393830d820cfdf3584c751641dd81f4ff
Ruan França <ruanzinr1000@hotmail.com>
2023-07-04 22:12:27 -0300

    repository pt1

--- src/main/java/br/com/buzzi/campeonatobrasileiro/entity/Jogo.java
+++ src/main/java/br/com/buzzi/campeonatobrasileiro/entity/Jogo.java
@@ -3,10 +3,7 @@ package br.com.buzzi.campeonatobrasileiro.entity;
 import lombok.Data;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -14,10 +11,15 @@ public class Jogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Time time1;
-    private Time time2;
     private Integer golsTime1;
     private Integer golsTime2;
     private Integer publicoPagante;
-    private String uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "time1")
+    private Time time1;
+
+    @ManyToOne
+    @JoinColumn(name = "time2")
+    private Time time2;
 }
--- src/main/java/br/com/buzzi/campeonatobrasileiro/entity/Time.java
+++ src/main/java/br/com/buzzi/campeonatobrasileiro/entity/Time.java
@@ -2,10 +2,7 @@ package br.com.buzzi.campeonatobrasileiro.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,7 +10,11 @@ public class Time {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(length = 20)
     private String nome;
+    @Column(length = 4)
     private String sigla;
+    @Column(length = 2)
     private String uf;
+    private String estadio;
 }
